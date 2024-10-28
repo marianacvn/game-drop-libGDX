84f4e8eebc574f7a18b23e7d41ddb4625baa8e9f
marianacvn <marianacvn1@gmail.com>
2024-10-28 13:33:25 -0300

    Criado projeto, primeira versão do jogo

---
+++ android/src/main/java/br/edu/demo/test/android/AndroidLauncher.java
@@ -0,0 +1,18 @@
+package br.edu.demo.test.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import br.edu.demo.test.Main;
+
+/** Launches the Android application. */
+public class AndroidLauncher extends AndroidApplication {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        AndroidApplicationConfiguration configuration = new AndroidApplicationConfiguration();
+        configuration.useImmersiveMode = true; // Recommended, but not required.
+        initialize(new Main(), configuration);
+    }
+}
